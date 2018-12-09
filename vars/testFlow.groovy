1206e7152392b04b90c3fcdc6edbea97790a5a52
Sunnygupta1401 <35125447+Sunnygupta1401@users.noreply.github.com>
2018-12-08 22:21:29 -0500

    Update testFlow.groovy

--- vars/testFlow.groovy
+++ vars/testFlow.groovy
@@ -19,7 +19,7 @@ def call(body) {
                     git branch: pipelineParams.branch, url: pipelineParams.scmUrl
                  echo pipelineParams.check
                  
-                echo "evaluate(readFile("${env.WORKSPACE}/prop.groovy"))"
+                evaluate(readFile("${env.WORKSPACE}/prop.groovy"))
                 // echo "${personDetails.firstName}"
 
 
