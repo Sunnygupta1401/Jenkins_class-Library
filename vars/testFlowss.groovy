7146e5a2e7598260cc55f8be8d3ec5e7cf768b02
Sunnygupta1401 <35125447+Sunnygupta1401@users.noreply.github.com>
2018-12-08 22:42:42 -0500

    Update testFlowss.groovy

--- vars/testFlowss.groovy
+++ vars/testFlowss.groovy
@@ -5,68 +5,55 @@ def call(body) {
     body()
     
     
-    pipeline {
-        agent any
-     tools
-     {
-      
-      maven 'localMaven'
-     }
-        stages {
-            stage('checkout git') {
-                steps {
-                    git branch: pipelineParams.branch, url: pipelineParams.scmUrl
+   node {
+   def mvnHome
+   def bb
+   def GIT_BRANCH
+   
+            stage('Preparation') {
+            git branch: pipelineParams.branch, url: pipelineParams.scmUrl
                  echo pipelineParams.check
-                 
+                       mvnHome = tool name: 'localMaven', type: 'maven'
+
                //readFile("${env.WORKSPACE}/prop.groovy")
-                 echo "${pipelineParams.personDetails.firstName}"
-
-
-                }
+                // echo "${pipelineParams.personDetails.firstName}"
             }
-
             stage('build') {
-                steps {
-                    sh 'mvn clean package package'
-                               // sh "'${mvnHome}/bin/mvn' clean package"
+               
+                    //sh 'mvn clean package package'
+                   sh "'${mvnHome}/bin/mvn' clean package"
 
                     echo "build"
-                }
+                
             }
 
             stage ('Archive') {
-                steps {
+               
            archiveArtifacts artifacts: '**/target/*.war', fingerprint: true
-                }
+                
             }
 
             
 
             stage('deploy staging'){
-                steps {
+                
 //build 'deploy-to-staging'
 
       //  sh 'cp **/target/*.war /Users/sunnygupta/Documents/apache-tomcat-8.5.20/webapps'
 
                  sh "cp **/target/*.war ${pipelineParams.stagingServer}"
 
-                }
+                
             }
 
             stage('deploy production'){
-                steps {
+                
 //build 'deploy-to-production'               
                  
             sh 'cp **/target/*.war     /Users/sunnygupta/Documents/apache-tomcat-8.5.20-production.20/webapps'
 
                  
-                }
+                
             }
         }
-        post {
-            failure {
-                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
-            }
-        }
-    }
-}
+        
