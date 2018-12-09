41080f18201ecc2dd68222ce830bf48a13e22d38
Sunnygupta1401 <35125447+Sunnygupta1401@users.noreply.github.com>
2018-12-08 21:47:55 -0500

    Update testFlow.groovy

--- vars/testFlow.groovy
+++ vars/testFlow.groovy
@@ -19,7 +19,7 @@ def call(body) {
                     git branch: pipelineParams.branch, url: pipelineParams.scmUrl
                  echo pipelineParams.check
                  
-                 echo "pipelineParams.personDetails"
+                 echo "${pipelineParams.personDetails}"
 
                 }
             }
